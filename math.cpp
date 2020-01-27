78bc05d7957682e75ff9a24278cb998e25dc61cb
Cory Brown <17816149+CoryBrown-42@users.noreply.github.com>
2020-01-27 12:34:46 -0500

    add tip function from last week
    selection and repetition structures

--- math.cpp
+++ math.cpp
@@ -4,3 +4,13 @@ int Add(int x, int y)
 {
 	return x + y;
 }
+
+int Sub(int x, int y)
+{
+	return x - y;
+}
+
+double Tip(double bill, double tipAmount, int people)
+{
+	return (bill * tipAmount) / people;
+}
